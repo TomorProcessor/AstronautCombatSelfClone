0dcb7bcd0433c52b220963246767902f297aa6fc
Tömördi Péter <tomordi.peter@zengo.eu>
2023-06-15 23:30:46 +0200

    initial state: 2016.09.20

---
+++ Assets/Scripts/CharacterMovementScript.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterMovementScript : MonoBehaviour {
+
+    public float speed = 2.0f;
+    private Rigidbody2D rigidbody2d;
+    private bool facingRight = true;
+    public float jumpForce = 2.0f;
+
+	// Use this for initialization
+	void Start () {
+
+        this.rigidbody2d = GetComponent<Rigidbody2D>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+
+
+        /*
+            A unitiben alapból definiált horizontal axes. unitiben az input managernél érjük el.
+            Az exes-ek külön is definiálhatóak. A float értékből fogjuk tudni, hogy merre megyünk.
+        */
+        float direction = Input.GetAxis("Horizontal");
+
+        //így kérünk le és módosítunk komponenst
+
+        this.rigidbody2d.velocity = new Vector2(direction*this.speed, rigidbody2d.velocity.y);
+
+
+        if (direction > 0 && !facingRight)
+        {
+            flip();
+        } else if (direction < 0 && facingRight)
+        {
+            flip();
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        { 
+            
+            this.rigidbody2d.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+        }
+	}
+
+
+    private void flip()
+    {
+        facingRight = !facingRight;
+
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
---
+++ Assets/Scripts/FireRocket.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireRocket : MonoBehaviour {
+
+    public Rigidbody2D rocket = null;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetButtonDown ("Fire1"))
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                fire();
+            }
+        } 
+	}
+
+    private void fire()
+    {
+        Rigidbody2D rocketClone = (Rigidbody2D)Instantiate(rocket, GetComponent<Transform>().position, rocket.transform.rotation);
+    }
+}
